c8d76fce24ab28f2f699126b7822012c90071bef
kbisht-codes <73687134+kbisht-codes@users.noreply.github.com>
2026-08-03 10:41:04 +0530

    Add: three-integer-sum - submission-8

---
+++ Data Structures & Algorithms/three-integer-sum/submission-8.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+        for(int i = 0; i< nums.size(); i++) {
+            int l = i + 1, r = nums.size() - 1;
+            
+            if(nums[i] > 0)
+                break;
+            if(i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            while(l < r) {
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum < 0)
+                    l++;
+                else if(sum > 0)
+                    r--;
+                else {
+                    ans.push_back({nums[i], nums[l], nums[r]});
+                    l++;
+                    r--;
+                    while(l < nums.size() && nums[l] == nums[l - 1])
+                        l++;
+                    while(r < nums.size() && nums[r] == nums[r + 1])
+                        r--;
+                }
+                
+            }
+        }
+        return ans;
+    }
+};
